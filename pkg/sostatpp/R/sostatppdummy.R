ec24b50fa7efcee2edc64c7c71e41abfa5109754
ute <ute@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-13 16:13:49 +0000

    Checking versions of plutils and fdnonpar
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fudapp@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sostatpp/R/sostatppdummy.R
+++ pkg/sostatpp/R/sostatppdummy.R
@@ -1,16 +1,24 @@
 #unloadNamespace(ns="fdnonpar")
-install.packages("fdnonpar", repos = "http://R-Forge.R-project.org")
+#install.packages("fdnonpar", repos = "http://R-Forge.R-project.org")
 #unloadNamespace(ns="plutils")
-install.packages("plutils", repos = "http://R-Forge.R-project.org")
+#install.packages("plutils", repos = "http://R-Forge.R-project.org")
+
 
 #'@title Hello world
 #'@description Say hello to everybody!
 #'@param message What you have to say, if anything
 #'@export helloworld
 #'@examples
+#'installed.packages()->IP
 #'helloworld("don't give up!")
-#
+#'helloworld(IP["plutils","Version"])
+#'helloworld(IP["fdnonpar","Version"])
+
 helloworld <- function(message = "dunno (???)")
 {
-   cat("Message of the day:", message,"\n--- that was basically it!")
+   cat("Message of the day:", message,"\n--- that was basically it!\n")
 }
+
+installed.packages()->IP
+helloworld(paste("plutils:", IP["plutils","Version"]))
+helloworld(paste("fdnonpar:", IP["fdnonpar","Version"]))
